24a84ff02d312183a7a81d00aaf8a56e07e0ad0b
Dmitriy Dudnik <ddudnik79@gmail.com>
2021-07-03 00:52:22 +0300

    Выполнено задание 2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,20 +1,32 @@
+import PropTypes from "prop-types";
 import css from './Statistics.module.css'
-//import PropTypes from "prop-types";
+ 
 
 
 export const Statistics = ({ title, stats }) => {
-  const stat = stats[0];
   return (
     <section className="statistics">
-      
-      <h2 className={css.title}>{title }</h2>
+     
+      <h2 className = { css.title } > { title }</h2>
 
-  <ul className="stat-list">
-    <li className="item">
-      <span className="label">{stat.label}</span>
-      <span className="percentage">{stat.percentage}</span>
-    </li>
-  </ul>
-</section>
-  )
-}
+      <ul className = { css.list } >
+        {stats.map(stat => (
+          
+          <li className={css.item} key={stat.id} >
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
+          </li>
+        ))}
+        
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  label: PropTypes.string,
+  percentage: PropTypes.number,
+
+
+};
